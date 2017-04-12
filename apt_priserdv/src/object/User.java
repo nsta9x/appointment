b6170e7dc2a3d0f4a0a8008868e8afab4136970e
leanhtu <leanhtu_2210@yahoo.com.vn>
2017-04-12 13:52:52 +0200

    a

---
+++ apt_priserdv/src/object/User.java
@@ -0,0 +1,13 @@
+package object;
+
+public class User {
+			public String realname;
+			public String sex;
+			
+			public User(String realname,  String sex){
+				this.realname = realname;
+				this.sex = sex;
+			}
+			
+			
+}
